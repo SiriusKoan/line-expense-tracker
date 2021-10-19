3e6d17fc847e511684ecd17d3f59bc0306793208
SiriusKoan <k.peihsun@gmail.com>
2021-10-19 10:22:05 +0800

    Fix dict error

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def handle_message(event):
         msg = ""
         records = list_records()
         for record in records:
-            msg += f"id: {record.id}, status: {record.status}, {record.debtor}: {record.money} -> {record.lender}\n"
+            msg += f"id: {record['id']}, status: {record['status']}, {record['debtor']}: {record['money']} -> {record['lender']}\n"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
     if command == "summary":
         summary = calculate_summary(message)
